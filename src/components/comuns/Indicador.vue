ef234fa2d6de8e7411e8730a64d8681ae6908d2e
SamuelPereiraBrandao <samuelpbrandao58@gmail.com>
2022-07-20 18:45:14 -0300

    add estilo visual computado

--- src/components/comuns/Indicador.vue
+++ src/components/comuns/Indicador.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+  <div :class="estilo">
     <p>{{titulo}}</p>
     <h2>{{indicador}}</h2>
   </div>
@@ -7,6 +7,11 @@
 <script>
 export default {
   name: "IndicadorDoSite",
-  props:['titulo','indicador'],
+  props:['titulo','indicador','bg','color'],
+  computed:{
+    estilo(){
+        return `h-100 p-5 rounded-3 border ${this.bg} ${this.color}`
+    }
+  }
 };
 </script>
--- src/components/views/HomePrin.vue
+++ src/components/views/HomePrin.vue
@@ -7,13 +7,13 @@
         </div>
         <div class="row mt-5">
             <div class="col-4">
-                <indicador titulo="Vagas abertas" indicador="100"></indicador>
+                <indicador titulo="Vagas abertas" indicador="100" bg="bg-dark" color="text-white"></indicador>
             </div>
             <div class="col-4">
-                <indicador titulo="Profissionais cadastrados" indicador="225"></indicador>
+                <indicador titulo="Profissionais cadastrados" indicador="225" bg="bg-dark" color="text-white"></indicador>
             </div>
             <div class="col-4">
-                <indicador titulo="Visitantes online" indicador="25"></indicador> 
+                <indicador titulo="Visitantes online" indicador="25" bg="bg-light" color="text-dark"></indicador> 
 
             </div>
         </div>
